19b201913ae268fb94f966ed52d8a5397965411a
basilic-kevin.guillet <kevin.guillet@epita.fr>
2020-06-18 14:03:55 +0200

    [LZW]: Fix some lzw compression error and add tests

--- kevin.guillet_LZW.py
+++ kevin.guillet_LZW.py
@@ -25,6 +25,23 @@ def error_format():
 
 
 
+##
+#   Write_zip_file(): Write the compressed version of the file in another file:
+##
+
+def write_zip_file(path, data):
+
+    output_name = path.split(".")[0]
+    output_name = output_name.split("/")[-1]
+    with open(output_name + ".lzw", "w") as file_out:
+
+        for bitwise in data:
+            file_out.write(bitwise)
+#            file_out.write("\n")
+
+
+
+
 
 ##
 #   dico_constructor(): Create the initial dictionnary from the file and return
@@ -33,14 +50,34 @@ def error_format():
 
 def dico_constructor(data):
     size = 0
-    dico = {}
+    dico = []
+    index = 0
 
     for char in data:
 
-        # Insert into dictionnary with ASCII value as key:
-        if ord(char) not in dico:
-            dico[ord(char)] = char
+        # Insert into dictionnary according to lexical order:
+        if (char not in dico) and char != '\n':
+
+            for i in range(size):
+                if ord(dico[i]) > ord(char):
+                    index = i
+                    break
+                else:
+                    index = size
+
+            dico = dico[:index] + [char] + dico[index:]
             size += 1
+
+
+    #append the special LZW character '%':
+    index = 0
+    for i in range(size):
+        if ord(dico[i]) > ord('%'):
+            index = i
+            break
+
+    dico = dico[:index] + ['%'] + dico[index:]
+    size += 1
 
     return size, dico
 
@@ -55,9 +92,9 @@ def to_bitwise(nb, nb_bits):
     bitwise = ""
 
     while (nb_bits > 0):
-        if nb > pow(2, nb_bits):
+        if nb >= pow(2, nb_bits - 1):
             bitwise += "1"
-            nb -= pow(2, nb_bits)
+            nb -= pow(2, nb_bits - 1)
         else:
             bitwise += "0"
         nb_bits -= 1
@@ -88,32 +125,33 @@ def lzw_zip(path):
 
         for char in data:
 
-            if (buf + char) in dico.values():
+            if char == '\n':
+                continue
+
+            if (buf + char) in dico:
                 buf += char
 
+                if dico.index(buf) >= pow(2, nb_bit):
+                    output += [to_bitwise(dico.index('%'), nb_bit)]
+                    nb_bit += 1
+
+
             elif (buf != ""):
-                output += [to_bitwise(list(dico.keys())[list(dico.values()).index(buf)], nb_bit)]
+                output += [to_bitwise(dico.index(buf), nb_bit)]
 
                 if dico_size <= pow(2, nb_bit):
-                    dico[dico_size] = buf
+                    dico += [buf]
                     dico_size += 1
-                else:
-                    output += ["%"]
-                    nb_bit += 1
 
                 buf = char
 
-        if buf in dico.values():
-            output += [to_bitwise(list(dico.keys())[list(dico.values()).index(buf)], nb_bit)]
+        if buf in dico:
+            output += [to_bitwise(dico.index(buf), nb_bit)]
 
 
 
-    output_name = path.split(".")[0]
-    output_name = output_name.split("/")[-1]
-    with open(output_name + ".lzw", "w") as file_out:
+    write_zip_file(path, output)
 
-        for bitwise in output:
-            file_out.write(bitwise)
 
 
 
@@ -128,6 +166,8 @@ def lzw_unzip(path):
     if (len(path) < 4 or path[-4:] != ".lzw"):
         error_format();
         return None
+    else:
+        print("Not implemented yet")
 
 
 
